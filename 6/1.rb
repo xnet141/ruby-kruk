980095ac5f24475895df12b0f07cd1a8c70863ae
Alex Erofeev <xnet141@gmail.com>
2025-05-26 02:49:51 +0300

    test ++

---
+++ 6/1.rb
@@ -0,0 +1,6 @@
+# ['alex', 'petr', 'max'].map {|n| n.upcase}
+#  => ["ALEX", "PETR", "MAX"] 
+
+# ['alex', 'petr', 'max'].map(&:upcase) # => из функционального программирования
+#  => ["ALEX", "PETR", "MAX"] 
+
