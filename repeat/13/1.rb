792e1c6a382feb96639c05e844fdaf26eea1763a
Alex Erofeev <xnet141@gmail.com>
2025-07-26 18:28:49 +0300

    les 13 raise

--- repeat/13/1.rb
+++ repeat/13/1.rb
@@ -16,8 +16,10 @@ def div(a, b)
     puts "Произошда ошибка класса #{e.class.name}:\n#{e.message}"
     if retries < max_retries
       retries += 1
-      sleep 2**retries
+      # sleep 2**retries
       retry # выполнить блок begin еще раз
+    else
+      raise e.class, "попробовали вызвать 3 раза этот код, но не вышло... "#=> породить исключение
     end
     
     # puts "unknown exception"
@@ -33,6 +35,11 @@ end
 
 #puts div(10, 2)
 puts '=====trying to divide by zero:'
-puts div(10, 0).inspect
+begin
+  puts div(10, 0)
+rescue => e
+  puts e.class.name
+  puts e.message
+end
 
 puts 'Hi!!!!'
