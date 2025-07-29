37c2bda342ce6196d26d6d74fe08de569465d2e2
Alex Erofeev <xnet141@gmail.com>
2025-07-30 00:27:18 +0300

    classes

--- repeat/classes/1.rb
+++ repeat/classes/1.rb
@@ -1,1 +1,25 @@
-для подключения файла в irb можно набрать "irb -r ./filename.rb" # запустится консоль с подключенным файлом
+#для подключения файла в irb можно набрать "irb -r ./filename.rb" # запустится консоль с подключенным файлом
+
+TEST = "main namespace"
+
+module MySuperLib
+  class Animal
+    TEST = "my constant"
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def Animal.class_method
+
+    end
+
+    def hello
+      puts ::TEST # :: => двоеточия нужны для обращения к константе вне модуля
+    end
+  end
+end
+
+obj = MySuperLib::Animal.new "test"
+obj.hello
