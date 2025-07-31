907548736b466a1697e3a34d1807d2252a09ecb0
Alex Erofeev <xnet141@gmail.com>
2025-07-31 22:33:47 +0300

    les 14 inst

--- repeat/14/proj/demo.rb
+++ repeat/14/proj/demo.rb
@@ -1,6 +1,22 @@
 class Demo
+  # attr_accessor :val
+  
+  # def initialize(val)
+  #   @val = 42
+  # end
+
   def self.run
     42
-  end 
+  end
+
+  def calc(a, b)
+    a * b
+  end
+
+  def my_arr
+    [1, 2, 3]
+  end
 end
+
+
 
--- repeat/14/proj/spec/demo_spec.rb
+++ repeat/14/proj/spec/demo_spec.rb
@@ -4,6 +4,18 @@ require_relative '../demo'
 
 # describe - содержит в себе набор тестов (в блоках it, specify)
 RSpec.describe Demo do # в блок describe передана ссылка на класс demo.
+  specify '#my_arr' do
+    # puts obj.val
+    obj = described_class.new
+    expect(obj.my_arr).to include(2)
+    # obj.val = 1
+  end
+
+  specify '#calc' do
+    obj = described_class.new
+    expect(obj.calc(2, 3)).to eq(6)
+    expect(obj).to be_an_instance_of(described_class) # be_an_instance_of проверяет является ли obj образцом класса
+  end
   # #my_method -- instance (метод образца класса)
   # .my_method -- class method
   specify '.run' do
