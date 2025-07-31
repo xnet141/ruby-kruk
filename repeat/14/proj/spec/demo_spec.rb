e2af181710ba8b09fb600ca500a8050a0a28627b
Alex Erofeev <xnet141@gmail.com>
2025-07-31 19:21:50 +0300

    les 14 style

--- repeat/14/proj/spec/demo_spec.rb
+++ repeat/14/proj/spec/demo_spec.rb
@@ -3,10 +3,13 @@ require_relative '../demo'
 #rspec . --format doc
 
 # describe - содержит в себе набор тестов (в блоках it, specify)
-RSpec.describe Demo do
-  it 'self.run' do
+RSpec.describe Demo do # в блок describe передана ссылка на класс demo.
+  # #my_method -- instance (метод образца класса)
+  # .my_method -- class method
+  specify '.run' do
+    # puts "My class is #{described_class.inspect}" # described_class работает, если мы его указали выше
     # вот тут мы записываем сам тест!
-    result = Demo.run
+    result = described_class.run
     expect(result).to eq(42)
   end
 end
