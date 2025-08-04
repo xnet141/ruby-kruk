23caac1553f800ca99f315db02da1a600549fd55
Alex Erofeev <xnet141@gmail.com>
2025-08-05 00:18:41 +0300

    les 16 exceptions ok

--- repeat/14/proj/test.rb
+++ repeat/14/proj/test.rb
@@ -11,3 +11,30 @@ puts s
 # Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each {|f| require f}
 
 puts Dir[File.dirname(__FILE__)].inspect
+
+
+class Error < StandardError
+  AlexError = Class.new(self)
+  ERRORS = AlexError
+end
+
+def inc
+  raise Error::ERRORS, "fffffffffffffffff"
+end 
+
+def foo
+  hh = {test: "test"}
+  puts "start"
+  inc    
+  puts "end"
+  puts hh[:test1]
+end
+
+# begin
+#   foo
+# rescue => e
+#   puts e.class.name
+#   puts e.message 
+# end
+
+foo
