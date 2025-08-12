c9a194a0712407db70b73cbae86bf574ec883a51
Alex Erofeev <xnet141@gmail.com>
2025-08-12 22:53:16 +0300

    classes lambda

--- repeat/classes/3/callable.rb
+++ repeat/classes/3/callable.rb
@@ -1,8 +1,42 @@
-my_proc = Proc.new { |msg| puts msg }
+# my_proc = Proc.new { |msg| puts msg }
+# my_proc = proc { |msg| puts msg }
+# my_proc.call("Hello world")
+# my_proc.call("Hell YouTube")
 
-my_proc.call("Hello world")
+# my_lambda = lambda { |msg| puts msg }
+# my_lambda = -> (msg) { puts msg } # stabby lambda, thin arrow
+# my_lambda.call("Hell Tiktok")
 
-my_proc.call("Hell YouTube")
+# def my_method(callable)
+#   result = callable.call("Hello world!!")
+
+#   puts "The result is #{result}"
+#   puts "I have called #{callable.inspect}!"
+# end
+
+# my_lambda = lambda do |msg| 
+#   puts msg 
+#   return msg
+# end
+
+# my_method(my_lambda)
+
+### !! LAMBDA ЛУЧШЕ ПРОЦЕДУРЫ !! ###
+
+
+def my_method(callable)
+  result = callable.call("Hello world!!", 1, 2, 3) # процедура игнорирует лишние аргументы
+ 
+  puts "The result is #{result}"
+  puts "I have called #{callable.inspect}!"
+end
+
+my_lambda = proc do |msg| # процедура делает return(выход) не только из самой себя, но и из метода, где был сделан вызов 
+  puts msg 
+  return msg
+end
+
+my_method(my_lambda)
 
 
 
