d5c7dcc93d5ad6da91c54b4e5bac7854eee672e4
Alex Erofeev <xnet141@gmail.com>
2025-08-14 16:53:33 +0300

    classes arr 1/2

---
+++ repeat/classes/4/insert-vs-unshift.rb
@@ -0,0 +1,20 @@
+require 'benchmark/ips'
+
+Benchmark.ips do |x|
+  x.report('Array#unshift') do
+    array = []
+    100_000.times { |i| array.unshift(i) }
+  end
+
+  x.report('Array#insert') do
+    array = []
+    100_000.times { |i| array.insert(0, i) }
+  end
+
+  x.report('Array#push') do
+    array = []
+    100_000.times { |i| array.push(i) }
+  end
+
+  x.compare!
+end
