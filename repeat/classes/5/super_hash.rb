47857144897cdf5d273373de27797271741fc2bc
Alex Erofeev <xnet141@gmail.com>
2025-08-24 09:21:01 +0300

    classes respond_to

--- repeat/classes/5/super_hash.rb
+++ repeat/classes/5/super_hash.rb
@@ -1,15 +1,32 @@
 class SuperHash < Hash
   def method_missing(method, *args, &block)
-    store_attribute(method, *args) if method.end_with?('=')
+    return store_attribute(method, *args) if method.end_with?('=')
+
+    return self[method] if self.has_key?(method)
+
+    super # вызываем method_missing на классе родителя
   end
+
+  def respond_to_missing?(method, include_private = false)
+    self.has_key?(method) || method.end_with?("=") || super
+  end
+
+  # def self
+  #   self.inspect
+  # end
 
   private
 
   def store_attribute(method, *args)
-    
+    key = method.to_s.delete_suffix('=')
+    self[key.to_sym] = args[0]
   end
 end
 
+# self указывает на объект s
 s = SuperHash.new
 s.key = "value"
-puts s.key
+s.name = "John"
+# puts s.key
+puts s.name
+puts s.respond_to?(:name=)
--- repeat/classes/5/test.rb
+++ repeat/classes/5/test.rb
@@ -12,3 +12,20 @@ roman = Roman.new
 
 puts roman.test "name", "lastname"
 puts roman.self
+puts roman.class
+
+puts "====================================="
+
+class Test
+
+end
+
+puts Test.class.superclass # Module
+
+test = Test.new
+
+puts test.class # Test
+puts test.class.class # Class
+puts test.class.class.superclass # Module
+puts test.class.superclass # Object
+puts test.class.superclass.superclass # BasicObject
