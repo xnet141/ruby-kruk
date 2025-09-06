dbc7b1adf4b640a5394c05f98db8a299b2f1b39b
Alex Erofeev <xnet141@gmail.com>
2025-09-06 16:39:40 +0300

    my +

--- my/run.rb
+++ my/run.rb
@@ -2,6 +2,15 @@
 
 print "Введите имя файла для поиска: "
 
-file_searh = gets.strip.downcase
+file_searh = STDIN.gets.strip.downcase
 
 puts file_searh
+
+Dir.glob("/home/**/#{file_searh}*.*") do |file| 
+    # puts true 
+      puts file
+    end
+
+puts "=" * 33
+puts "d: #{d.inspect}"
+
