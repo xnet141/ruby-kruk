bdf0520d28746d3ce7292f38fdf7826ffc2b426e
Alex Erofeev <xnet141@gmail.com>
2025-09-19 18:45:44 +0300

    classes xor case

---
+++ repeat/classes/10/xor/1.rb
@@ -0,0 +1,21 @@
+
+puts "test" 
+
+puts "test".bytes
+
+puts "test".bytes.map {|i| i.to_s(2)}
+
+puts "="* 20
+
+puts "тест".bytes
+
+puts "="* 20
+
+puts "тест".bytes.map {|i| i.to_s(2)}
+
+puts "="* 20
+
+puts "тест".unpack("U*")
+
+puts "==="
+
