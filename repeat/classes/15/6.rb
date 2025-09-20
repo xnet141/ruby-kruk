4d549939fb35b85327390797f4e8ce9b5530cf01
Alex Erofeev <xnet141@gmail.com>
2025-09-20 23:54:27 +0300

    classes xor pin

--- repeat/classes/15/6.rb
+++ repeat/classes/15/6.rb
@@ -1,9 +1,36 @@
-ages = [28, 30, 35, 42, 50]
-sherlock_age = 30
+# ages = [28, 30, 35, 42, 50]
+# sherlock_age = 30
 
-case ages
-in watson_age, ^sherlock_age, *other_ages # ^(оператор пин - каретка или домик) -> закрепляет значение sherlock_age, соответственно, если сопоставление не сработает, то выдаст ошибку
-  puts watson_age
-  puts sherlock_age
-end
+# case ages
+# in watson_age, ^sherlock_age, *other_ages # ^(оператор пин - каретка или домик) -> закрепляет значение sherlock_age, соответственно, если сопоставление не сработает, то выдаст ошибку
+#   puts watson_age
+#   puts sherlock_age
+# end
 
+# users = [:ok,
+#   [
+#     {
+#       name: "Sherlock",
+#       surname: "Holmes",
+#       extra: {
+#         age: "30",
+#         hobby: "violin"
+#       }
+#     },
+#     {
+#       name: "John",
+#       surname: "Watson",
+#       extra: {
+#         age: "28",
+#         hobby: "writting"
+#       }
+#     }
+#   ]
+# ]
+
+# name = "John"
+
+# case users
+# in :ok, [*, {name: ^name, extra: {age:}}, *]
+#   puts age
+# end
